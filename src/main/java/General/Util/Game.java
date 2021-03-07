0f75ece8c4af1b30aea1284fbca179eda4217206
GentleLoud <luitmeinen@gmail.com>
2021-03-07 22:16:27 +0100

    Completely refactored the bot to make it future proof. It now tracks players and player list, ensuring only the player whose turn it is has any influence on the round

---
+++ src/main/java/General/Util/Game.java
@@ -0,0 +1,124 @@
+package General.Util;
+
+import com.google.inject.internal.Nullable;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.Arrays;
+
+public abstract class Game {
+
+    //Type of the current game
+    String type;
+
+    //button arrays
+    protected String[] continueButton = {"✅"};
+    protected String[] joinReactions = {"➕", "🍻"};
+    protected String[] newRoundReactions = {"❌", "✅"};
+
+    //the channel where all the messages for this game will be sent and the message IDs
+    public TextChannel channel;
+    public long gameMessageID;
+    public Long stopMessageID;
+
+    //variables for collecting players
+    public boolean collecting = false;
+    public User startUser;
+    public User currentUser;
+
+    public Game(TextChannel channel, String type){
+        this.channel = channel;
+        this.type = type;
+    }
+
+    //player tracking methods
+    public void setPlayerList(User user){
+        collecting = true;
+        Builders.sendEmbed(channel,
+                "Nieuwe ronde " + type,
+                user.getAsMention() + " begint een nieuwe ronde " + type + ".",
+                "Reageer met \"➕\" op dit bericht om mee te doen en met \"" + getEmote(type) + "\" als iedereen heeft gereageerd.",
+                joinReactions,
+                true,
+                false,
+                false);
+        UserList.createUserList(channel);
+        startUser = user;
+        currentUser = startUser;
+        UserList.addUser(channel, user);
+    }
+
+    public void addPlayer(User user){
+        UserList.addUser(channel, user);
+        channel.retrieveMessageById(gameMessageID).queue(msg -> {
+            String playerString = "";
+            for(User listUser : UserList.getUserList(channel)){
+                if(listUser != startUser) {
+                    playerString += listUser.getAsMention() + " \n";
+                }
+            }
+            Builders.updateGameEmbed(
+                    msg,
+                    "Nieuwe ronde " + type,
+                    startUser.getAsMention() + " begint een nieuwe ronde " + type + ". Momenteel doen de volgende users ook mee: \n" + playerString,
+                    "Reageer met \"➕\" op dit bericht om ook mee te doen en reageer met \"" + getEmote(type) + "\" als alle deelnemers in de lijst staan.",
+                    joinReactions
+            );});
+    }
+
+    public void startGame(){
+        collecting = false;
+        startTurn();
+    }
+
+    //Round/turn voids.
+    //A turn is someones turn from start to finish. A round is a single round within such a turn.
+    public abstract void startTurn();
+
+    public abstract void updateTurn();
+
+    public abstract void updateRound(@Nullable String reaction);
+
+    public abstract void processReaction(String reaction, User user);
+
+    public abstract void resetRound();
+
+    public abstract void confirm(User user);
+
+    //for when one player is done with their turn and some values need to be reset.
+    public abstract void startNextTurn();
+
+    public abstract void gameDone();
+    //util
+    public String[] sortedReactions(@Nullable String[] reactions) {
+        if (reactions != null) {
+            if (reactions.length > 2) {
+                String[] numbers = Arrays.copyOfRange(reactions, 1, reactions.length - 1);
+                Arrays.sort(numbers);
+                System.arraycopy(numbers, 0, reactions, 1, reactions.length - 1 - 1);
+            }
+        }
+        return reactions;
+    }
+
+    //getters
+    public String getEmote(String type){
+        String emote = "";
+        switch (type){
+            case "dertigen":
+                emote = "🍻";
+                break;
+            case "mexen":
+                emote = "🎲";
+                break;
+        }
+        return emote;
+    }
+
+    //setters
+    public void setGameMessageID(Message gameMessage) {gameMessageID = gameMessage.getIdLong();}
+
+    public void setStopMessageID(Message stopMessage) {stopMessageID = stopMessage.getIdLong();}
+
+}
---
+++ src/main/java/General/Util/UserList.java
@@ -0,0 +1,41 @@
+package General.Util;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserList {
+    private static final HashMap<MessageChannel, List<User>> userList = new HashMap<>();
+
+    public static void createUserList(MessageChannel messageChannel) {
+        List<User> users = new ArrayList<>();
+        userList.put(messageChannel, users);
+    }
+
+    public static void addUser(MessageChannel messageChannel, User user) {userList.get(messageChannel).add(user);}
+
+    public static User getNextUser(MessageChannel messageChannel, User user) {
+        List<User> users = userList.get(messageChannel);
+        int currentID = users.indexOf(user);
+        if((users.size() - 1) != currentID){
+            return users.get(currentID + 1);
+        }else{
+            return users.get(0);
+        }
+    }
+
+    public static void removeUser(MessageChannel messageChannel, User user) {userList.get(messageChannel).remove(user);}
+
+    public static boolean contains(MessageChannel channel, User user){return userList.get(channel).contains(user);}
+
+    public static boolean hasUserList(MessageChannel messageChannel) {return userList.containsKey(messageChannel);}
+
+    public static List<User> getUserList(MessageChannel messageChannel) {return userList.get(messageChannel);}
+
+    public static void removeUserList(MessageChannel messageChannel) {
+        userList.remove(messageChannel);
+    }
+}
